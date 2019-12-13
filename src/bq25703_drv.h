111c5ecfc65f80af03c33648b331215213f1f7d4
link.lin <Link.Lin@tymphany.com>
2019-12-13 18:27:33 +0800

    remove unused code

--- src/bq25703_drv.h
+++ src/bq25703_drv.h
@@ -33,26 +33,6 @@
 #define INPUT_VOLTAGE_LIMIT_3V2   0x0000 //3200mV
 
 
-
-#define CHARGE_VOL_1    11  // 11008 mv
-#define CHARGE_VOL_2    12  // 12000 mv
-#define CHARGE_VOL_3    13  // 13008 mv
-#define CHARGE_CUR_1    1   // 1024 mA 
-#define CHARGE_CUR_2    2   // 2048 mA
-#define CHARGE_CUR_3    3   // 3008 mA
-
-
-#define OTG_VOL_1   5   // 4992 mv
-#define OTG_VOL_2   9   // 9024 mv
-#define OTG_VOL_3   15  // 15040 mv
-#define OTG_CUR_1   1   // 1024 mA
-#define OTG_CUR_2   2   // 2048 mA
-#define OTG_CUR_3   3   // 3008 mA
-
-//do_what
-#define CHARGE_FUNCTION 0
-#define OTG_FUNCTION    1
-
 #define CHG_OK_PIN      106
 
 
